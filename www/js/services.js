21c531f4ac16e8b964da55a8a9c23658ef104a23
xuxl <xuxle@yonyou.com>
2015-06-26 11:21:31 +0800

    change file code style

--- www/js/services.js
+++ www/js/services.js
@@ -10,6 +10,6 @@ define(['ionic'], function () {
             PLATFORM_BACK_BUTTON_PRIORITY_VIEW: 110,
             EXIT_APP_CONFIRM_TIME: 2000,
 
-            EXIT_APP_CONFIRM_STR: '�ٰ�һ���˳�����վ'
+            EXIT_APP_CONFIRM_STR: '再按一次退出工作站'
         });
 });
